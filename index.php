f04b4022f061a9bf33aaa4ae5d0d12649d9272e7
Nemolite <g25092011@mail.ru>
2021-01-17 17:45:22 +0300

    Хидер

--- index.php
+++ index.php
@@ -14,132 +14,27 @@
 
 get_header();
 ?>
-	<div class="main">
-
-
- 		<header class="main__head">
-	    <nav class="navbar menu-color">
-        <div class="container-fluid">
-    
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed fix-btn-color" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>      
-    </div>   
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">	
-    <?php 
-	    wp_nav_menu( array(
-        'menu' => 'top_row_menu',
-        'theme_location'  => 'top_row_menu', 
-	      'depth' =>  2,
-	      'container' => false,
-	      'strcasecmp' => 1,
-	      'menu_class' => 'nav navbar-nav center',
-	      'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
-	      'walker' => new wp_bootstrap_navwalker())
-      );
-
-      wp_nav_menu( array(
-        'menu' => 'top_row_menu',
-        'theme_location'  => 'top_row_menu', 
-        'depth' =>  0,
-        'container' => false,
-        'menu_class' => 'dropdown-menu center',
-        'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
-        'walker' => new wp_bootstrap_navwalker())
-      );
-    ?>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Что искать?" value="<?php echo get_search_query() ?>" name="s" id="s">
-        </div>
-        <button type="submit" class="btn btn-default">Найти</button>
-      </form>
-
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-          <a href="#" id="hover-fix-color-top" >Версия для слабовидящих</a>
-        </li>
-        
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav> 
-		</header>
-
-		<div class="content">
-	    <div class="content_images">
-            <img src="<?php echo bloginfo('template_url'); ?>/vendor/images/header2.jpeg" alt=""> 
-            <h1>
-			<?php echo ot_get_option( 'head_main_text' );?>
-			
-			</h1>
-    </div>
-
-<nav class="navbar middle-menu-color">
-  <div class="container-fluid">
-   
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed fix-btn-color-top" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-     
-    </div>
-
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <?php 
-	    wp_nav_menu( array(
-        'menu' => 'middle_row_menu',
-        'theme_location'  => 'middle_row_menu', 
-	      'depth' =>  2,
-	      'container' => false,
-	      'strcasecmp' => 1,
-	      'menu_class' => 'nav navbar-nav center',
-	      'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
-	      'walker' => new wp_bootstrap_navwalker())
-      );
-
-      wp_nav_menu( array(
-        'menu' => 'middle_row_menu',
-        'theme_location'  => 'middle_row_menu', 
-        'depth' =>  0,
-        'container' => false,
-        'menu_class' => 'dropdown-menu center',
-        'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
-        'walker' => new wp_bootstrap_navwalker())
-      );
-    ?>     
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
 <div class="content_wlecome">
- <div class="row">
-  <div class="col-md-4 col-sm-4 col-xs-12">
-       <div class="welcome_images">
-          <img src="<?php echo bloginfo('template_url'); ?>/vendor/images/topmanager.jpg" alt=""> 
-       </div>
-  </div>
-  <div class="col-md-8 col-sm-8 col-xs-12">
-        <div class="welcome_text">
-      <h1>
-	   <?php echo ot_get_option( 'head_welcome_text' );?>     
-    </h1>
-	<?php echo ot_get_option( 'welcome_textarea' );?>
-
-    <p class="statment">
-      <?php echo ot_get_option( 'footer_welcome_text' );?>
-    </p>
-       </div>
-  </div>
+<div class="row">
+<div class="col-md-4 col-sm-4 col-xs-12">
+<div class="welcome_images">
+ <img src="<?php echo bloginfo('template_url'); ?>/vendor/images/topmanager.jpg" alt=""> 
 </div>
-    
+</div>
+<div class="col-md-8 col-sm-8 col-xs-12">
+<div class="welcome_text">
+<h1>
+<?php echo ot_get_option( 'head_welcome_text' );?>     
+</h1>
+<?php echo ot_get_option( 'welcome_textarea' );?>
+
+<p class="statment">
+<?php echo ot_get_option( 'footer_welcome_text' );?>
+</p>
+</div>
+</div>
+</div>
+
 </div>  <!-- class="content_wlecome" -->
 
 <!-- Верхний ряд банеров, сделаны на Option Tree (см. в thema-option.php папка template-parts)-->

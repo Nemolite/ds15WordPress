b4e2488a11a1f324050bdb70ed8dff22053e1bcb
Nemolite <g25092011@mail.ru>
2021-03-11 10:11:04 +0300

    Изменения в видеоконтенте

--- inc/content-photo.php
+++ inc/content-photo.php
@@ -6,7 +6,7 @@
 <div class="content_photo">
   <h1>Наша фотогалерея</h1>  
     <div class="photo-galery">    
-    <?php echo do_shortcode('[metaslider id="795"]'); ?>
+    <?php echo do_shortcode('[metaslider id="795"]'); ?> <!-- 885 -->
     </div><!-- class="photo-galery"-->
     <a href="https://disk.yandex.ru/d/fRaR01ie3OcaUQ?w=1" target ="_blank">
       <div class="content_photo_btn">
--- inc/content-video.php
+++ inc/content-video.php
@@ -6,16 +6,10 @@
 <div class="content_video">
   <h1>Наша видеотека</h1>
   <div class="content_video-youtube">
-    <div class="row">
+    <div class="row fix_content_video-youtube">
     <div class="col-md-3 col-xs-12 col-sm-3">
         <div class="youtube-mini">
-        <div class="youtube-mini-top">
-        <iframe width="100%" height="100%" src="<?php echo ot_get_option( 'youtube_1' );?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-        </div>
-        <div class="youtube-mini-bottom">
-        <iframe width="100%" height="100%" src="<?php echo ot_get_option( 'youtube_2' );?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
-        </div>          
+                  
         </div>
       </div>
       <div class="col-md-6 col-xs-12 col-sm-6">
@@ -24,16 +18,15 @@
         </div>
       </div>
       <div class="col-md-3 col-xs-12 col-sm-3">
-        <div class="youtube-mini">
-        <div class="youtube-mini-top">
-        <iframe width="100%" height="100%" src="<?php echo ot_get_option( 'youtube_4' );?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        <div class="youtube-mini-bottom">
-        <iframe width="100%" height="100%" src="<?php echo ot_get_option( 'youtube_5' );?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div> 
+        <div class="youtube-mini">        
           
         </div>
       </div>
     </div>
+    <a href="https://www.youtube.com/channel/UCfIOeLvA1wmPxT9nloKb1cQ" target ="_blank">
+      <div class="content_video-youtube-btn">
+      <p>Посетите нашу видеотеку</p>
+      </div>
+    </a>
   </div><!-- class="content_video-youtube"-->
 </div><!-- class="content_video"-->

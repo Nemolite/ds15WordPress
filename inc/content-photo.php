f5f8abfe6ae7aeb2287264b70f40e4182f03754e
Nemolite <g25092011@mail.ru>
2021-03-11 09:35:42 +0300

    Изменение раздела фотогалереии

--- inc/content-photo.php
+++ inc/content-photo.php
@@ -4,9 +4,13 @@
  */
 ?>
 <div class="content_photo">
-  <h1>Наша фотогалерея</h1>
-  <p><a href="#">Посетите нашу фотогалерею</a></p>
+  <h1>Наша фотогалерея</h1>  
     <div class="photo-galery">    
-    <?php echo do_shortcode('[metaslider id="795"]'); ?>
+    <?php echo do_shortcode('[metaslider id="795"]'); ?> <!-- 885 -->
     </div><!-- class="photo-galery"-->
+    <a href="https://disk.yandex.ru/d/fRaR01ie3OcaUQ?w=1" target ="_blank">
+      <div class="content_photo_btn">
+          <p>Посетите нашу фотогалерею</p>
+      </div> 
+    </a>
 </div><!-- class="content_photo"" -->

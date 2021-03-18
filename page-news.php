62a91cda9e0e788a715bf234ab2b05f196d434cb
Nemolite <g25092011@mail.ru>
2021-03-18 11:15:34 +0300

    Добавление фона на страницу новостей

--- page-news.php
+++ page-news.php
@@ -7,13 +7,13 @@ get_header();
 <div class="page_news">
     <h1 class="content-page-title">Новости</h1>
 <div class="row">
-  <div class="col-md-8">
+  <div class="col-md-offset-1 col-md-8">
    <?php 
    // Получение списка всех новостей   
     do_action('display_all_news',10);
    ?>  
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3">
  <?php get_sidebar(); ?>
   </div>
 </div>
